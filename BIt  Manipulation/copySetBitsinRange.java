3dda9f58e8637bb0bc4c0bb546d2fad51916d00d
Raghav Bajaj <73180455+Raghav-Bajaj@users.noreply.github.com>
2021-06-27 23:10:11 +0530

    Create copySetBitsinRange.java

---
+++ BIt  Manipulation/copySetBitsinRange.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int a = scn.nextInt();
+    int b = scn.nextInt();
+    int left = scn.nextInt();
+    int right = scn.nextInt();
+
+   int mask = (1 << right-left+1) ;
+   mask-- ;
+   mask = mask <<left-1 ;
+   mask = mask&a;
+   b = b | mask ;
+   System.out.println(b);
+    
+  }
+
+}
