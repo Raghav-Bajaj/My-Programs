4a5c55bf1a92af6a006c28b570d25ef6b9e8885a
Raghav Bajaj <73180455+Raghav-Bajaj@users.noreply.github.com>
2021-07-12 20:03:35 +0530

    Create NegativeWeightCycleDetection.java
    You are given 2 integers N and M , N is the number of vertices, M is the number of edges. You'll also be given ai, bi and wi where ai and bi represents an edge from a vertex ai to a vertex bi and wi respresents the weight of that edge.
    
    Task is to find if it contains a negative weight cycle or not.
    
    
    Print 1 if it contains a negative weight cycle else print 0.

---
+++ graphs/level-Up/NegativeWeightCycleDetection.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+        Scanner sc = new Scanner(System.in);
+        int v = sc.nextInt();
+        int e = sc.nextInt();
+        int[][] graph = new int[e][3];
+        for(int i = 0 ; i < e ; i++)
+        {
+            graph[i][0] = sc.nextInt() ;
+             graph[i][1] = sc.nextInt() ;
+              graph[i][2] = sc.nextInt();
+            
+        }
+        int[] arr = new int[v];
+        Arrays.fill(arr,Integer.MAX_VALUE);
+        arr[0] = 0 ;
+        for(int i = 0 ; i < v-1 ; i++)
+        {
+            for(int j = 0 ; j < e ; j++)
+            {
+                int u = graph[j][0];
+                int d = graph[j][1];
+                int w = graph[j][2];
+                if(arr[u] != Integer.MAX_VALUE)
+                {
+                    if(arr[u] + w < arr[d])
+                    {
+                        arr[d] = arr[u] + w ;
+                    }
+                }
+                
+            }
+        }
+        for(int j = 0 ; j < e ; j++)
+            {
+                int u = graph[j][0];
+                int d = graph[j][1];
+                int w = graph[j][2];
+                if(arr[u] != Integer.MAX_VALUE)
+                {
+                    if(arr[u] + w < arr[d])
+                    {
+                        System.out.println("1");
+                        return ;
+                    }
+                }
+                
+            }
+         System.out.println("0");
+			}
+}
