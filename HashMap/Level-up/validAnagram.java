6f2a95437671dd27f7c3b81ac5f2533aaa2c961c
Raghav Bajaj <73180455+Raghav-Bajaj@users.noreply.github.com>
2021-07-15 17:21:25 +0530

    Valid Anagram
    1. You are given two strings s1 and s2.
    2. You have to determine if s2 is an anagram of s1.

---
+++ HashMap/Level-up/validAnagram.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+	
+	public static boolean solution(String s1, String s2){
+	    if(s1.length()  != s2.length())
+	            return false ;
+	HashMap<Character,Integer> map = new HashMap<>();
+	for(int i = 0 ; i < s1.length() ; i++ )
+	{
+	    char ch = s1.charAt(i);
+	    map.put(ch , map.getOrDefault(ch , 0) + 1 ) ;
+	}
+
+    for(int i = 0 ; i < s2.length() ; i++ )
+	{
+	    char ch = s2.charAt(i);
+	    if(map.containsKey(ch))
+	    {
+	        if(map.get(ch) == 1 )
+	        {
+	            map.remove(ch);
+	        }else{
+	            map.put(ch , map.getOrDefault(ch , 0) -1 ) ;
+	        }
+	    }else{
+	        return false ;
+	    }
+	    
+	}
+		return map.size() == 0 ;
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String s1 = scn.next();
+		String s2 = scn.next();
+		System.out.println(solution(s1,s2));
+	}
+
+}
