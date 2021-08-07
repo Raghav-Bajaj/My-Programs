efe3fe836f2686d669088af435db2b9d5db1f743
Raghav Bajaj <73180455+Raghav-Bajaj@users.noreply.github.com>
2021-08-07 22:56:02 +0530

    Allocate Minimum Number Of Pages

---
+++ Arrays and String/Allocate Minimum Number Of Pages.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  public static int minPages(int[]arr, int m) {
+   int lo = 0 ;
+   int hi = 0 ;
+   for(int i = 0 ; i  < arr.length ; i++)
+   {
+       hi += arr[i] ;
+       lo = Math.max(lo,arr[i]);
+   }
+   if(m == 1)
+        return hi ;
+    if(m == arr.length)
+        return lo ;
+    int res = Integer.MAX_VALUE ;
+    while(lo <= hi)
+    {
+        int mid = lo +(hi-lo)/2 ;
+        int count = 1;
+        int temp = 0 ;
+        for(int i = 0 ; i  < arr.length ; i++)
+       {
+               temp += arr[i] ;
+               if(temp > mid)
+               {
+                   count++ ;
+                   temp = arr[i];
+               }
+        }
+        if(count <= m)
+        {
+            res = Math.min(res,mid);
+            hi = mid -1 ;
+        }else{
+            lo = mid+1 ;
+        }
+        
+    }
+    return res;
+  }
+
+  public static void main(String[]args) {
+    //input work
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    int[]arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+
+    int m = scn.nextInt();
+
+    int ans = minPages(arr, m);
+    System.out.println(ans);
+  }
+}
