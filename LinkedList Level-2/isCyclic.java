2128570db12a6c988b48c4bf6fd07a362f9b08e4
Raghav Bajaj <73180455+Raghav-Bajaj@users.noreply.github.com>
2021-08-11 18:01:58 +0530

    Create isCyclic.java

---
+++ LinkedList Level-2/isCyclic.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Main {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static boolean isCyclePresentInLL(ListNode head) {
+    ListNode fast = head ;
+    ListNode slow = head ;
+    while(fast.next != null && fast.next.next != null)
+    {
+        fast = fast.next.next ;
+        slow = slow.next ;
+        if(fast == slow)
+            return true ;
+    }
+        return false;
+    }
+
+    public static ListNode takeInput() {
+        int n = scn.nextInt();
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+        int idx = scn.nextInt();
+        if (idx >= 0) {
+            ListNode curr = dummy.next;
+            while (idx-- > 0) {
+                curr = curr.next;
+            }
+            prev.next = curr;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = takeInput();
+        System.out.println(isCyclePresentInLL(head));
+    }
+}
